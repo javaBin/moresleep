166bd795aaa84b12e71250b1f2d0c602063a3474
Anders Karlsen <karlsenanders@gmail.com>
2024-07-04 15:57:55 +0200

    Change name on enviroment. Fix error in dats in config

--- src/main/kotlin/no/java/moresleep/conference/ReadConfigCommand.kt
+++ src/main/kotlin/no/java/moresleep/conference/ReadConfigCommand.kt
@@ -8,8 +8,8 @@ import no.java.moresleep.UserType
 class ReadConfig(
     val conferenceUrl:String="https://sleepingpill.javazone.no/public/allSessions/javazone_2024",
     val conferenceName:String="JavaZone 2024",
-    val conferenceDates:List<String> = listOf("04.09.2023","05.09.2023"),
-    val workshopDate:String="03.09.2023"
+    val conferenceDates:List<String> = listOf("04.09.2024","05.09.2024"),
+    val workshopDate:String="03.09.2024"
 ):ServiceResult()
 
 class ReadConfigCommand:Command {
