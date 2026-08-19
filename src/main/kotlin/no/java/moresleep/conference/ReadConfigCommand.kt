5ff8efac6518aef9d3593d45d38f99a5d039274e
Anders Karlsen <karlsenanders@gmail.com>
2026-08-19 18:21:15 +0200

    Added 2026 config dates

--- src/main/kotlin/no/java/moresleep/conference/ReadConfigCommand.kt
+++ src/main/kotlin/no/java/moresleep/conference/ReadConfigCommand.kt
@@ -6,10 +6,10 @@ import no.java.moresleep.SystemUser
 import no.java.moresleep.UserType
 
 class ReadConfig(
-    val conferenceUrl:String="https://sleepingpill.javazone.no/public/allSessions/javazone_2025",
-    val conferenceName:String="JavaZone 2025",
-    val conferenceDates:List<String> = listOf("03.09.2025","04.09.2025"),
-    val workshopDate:String="02.09.2025"
+    val conferenceUrl:String="https://sleepingpill.javazone.no/public/allSessions/javazone_2026",
+    val conferenceName:String="JavaZone 2026",
+    val conferenceDates:List<String> = listOf("02.09.2026","03.09.2026"),
+    val workshopDate:String="01.09.2026"
 ):ServiceResult()
 
 class ReadConfigCommand:Command {
